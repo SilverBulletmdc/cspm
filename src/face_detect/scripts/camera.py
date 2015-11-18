6a14ed2cede3cdf5dab8be12cb6e39380ce0a68e
SilverBulletmdc <mengdechaolive@qq.com>
2015-11-19 02:04:36 +0800

    aa

--- src/face_detect/scripts/camera.py
+++ src/face_detect/scripts/camera.py
@@ -6,37 +6,43 @@ import cv2
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+import color
+import geometry_msgs.msg
 
 class image_converter:
 
   def __init__(self):
-    self.image_pub = rospy.Publisher("image_topic_2",Image,queue_size = 50)
 
-    cv2.namedWindow("Image window", 1)
+    self.cmd_pub = rospy.Publisher("/cmd_vel",geometry_msgs.msg.Twist,queue_size= 1)
+    self.image_sub = rospy.Subscriber("/camera/image",Image,self.callback,queue_size = 1 )
+    cv2.namedWindow("Image window", cv2.CV_WINDOW_AUTOSIZE)
     self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/camera/image",Image,self.callback)
+
 
   def callback(self,data):
+    twist = geometry_msgs.msg.Twist()
     try:
       cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError, e:
       print e
 
     (rows,cols,channels) = cv_image.shape
-    if cols > 60 and rows > 60 :
-      cv2.circle(cv_image, (50,50), 10, 255)
-
-    cv2.imshow("Image window", cv_image)
+    (vx,vy,vz,wx,wy,wz) = color.action(cv_image,640,360)
+    print (vx,vy,vz,wx,wy,wz)
+    twist.linear.x = 0
+    twist.linear.y = vy
+    twist.linear.z = vz
+    twist.angular.x = wx
+    twist.angular.y = wy
+    twist.angular.z = wz
+    self.cmd_pub.publish(twist)
+    #cv2.imshow("Image window", cv_image)
     cv2.waitKey(3)
 
-    try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError, e:
-      print e
 
 def main(args):
   ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
+  rospy.init_node('color_tracker', anonymous=True)
   try:
     rospy.spin()
   except KeyboardInterrupt:
